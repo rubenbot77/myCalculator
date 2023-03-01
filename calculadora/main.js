8376ee851baac818ab25e2b20b41b34d5ecafbb4
rubenbot77 <mechasBRS707@hotmail.com>
2023-03-01 17:51:50 -0500

    fail and new process

--- calculadora/main.js
+++ calculadora/main.js
@@ -1,56 +1,95 @@
-const offBtn = document.querySelector('#off');
-const perBtn = document.querySelector('#per');
-const acBtn = document.querySelector('#ac');
-const cBtn = document.querySelector('#c');
-const screen = document.querySelector('.screen-contain')
-let buttons = [...document.querySelectorAll('.numbers')];
-let acumulate = [];
+const acumulate = document.querySelector('#acumulate-value');
+const current = document.querySelector('#current-value');
+const numbersBtn = document.querySelectorAll('.numbers');
+const operators = document.querySelectorAll('.operator');
 
 
-specials()
-function specials (){
-    screen.value = 'off';
-    if (screen.value = 'off') {
-        acumulate = ''
+
+numbersBtn.forEach(button => {
+    button.addEventListener('click', () => display.addNumber(button.innerHTML))
+});
+operators.forEach(operator => {
+    operator.addEventListener('click', ()=> display.comput(operator.value))
+})
+class Calculator {
+    add(num1, num2) {
+        return num1 + num2;
     }
-    buttons.forEach((button) => {
-        button.addEventListener('click', function (){
-            acumulate.push(button.value)
-            screen.innerText = acumulate.join('')
-            let max = acumulate.length
-            console.log(max);       
-            if (max >= 9) {
-               acumulate = ''
-            }
-             else {
-                return
-            }
-        })
-    })
-    offBtn.addEventListener('click', specialOff)
-    perBtn.addEventListener('click', specialPer)
-    acBtn.addEventListener('click', specialAc)
-    cBtn.addEventListener('click', specialC);
-    
-    function specialOff() {
-        screen.value = 'off';
-        screen.innerText = ''
-        acumulate = [];
+    subtract(num1, num2) {
+        return num1 - num2;
     }
-    function specialPer(){
-        console.log(screen.value);
+    divide(num1, num2){
+        return num1 / num2;
     }
-    function specialAc(){
-        screen.value = 'on';
-        screen.innerText = '0'
-        acumulate = [];
+    multiply(num1, num2){
+        return num1* num2
     }
-    function specialC(){
-        acumulate = []
-        if (screen.value == 'off') {
-            screen.innerText = '';      
-        } else {
-            screen.innerText = '0'
-        }
+    persentage(num1, num2){
+        return ((num1 * 100) / num2)
+    }
+    exponential(num1, num2) {
+        return num1 ** num2
     }
 }
+
+class Display {
+    constructor(current, acumulate){
+        this.current = current;
+        this.acumulate = acumulate;
+        this.calculator = new Calculator();
+        this.operatorType = undefined;
+        this.currentValue = '';
+        this.acumulateValue = '';
+        this.signs = {
+          add: '+',
+          divide: '÷',
+          subtract: '-',
+          multiply: 'x',
+          persentage: '%',
+          exponential: '^'
+        }
+    }
+    deleteAll(){
+        this.currentValue = '';
+        this.acumulateValue = '';
+        this.operatorType = undefined
+        this.printValue()
+    }
+    comput(type){
+        this.operatorType !== 'equal' && this.calculate();
+        this.operatorType = type;
+        this.acumulateValue = this.currentValue || this.acumulateValue;
+        this.currentValue = '';
+        this.printValue();
+    }
+    delete(){
+        this.currentValue = this.currentValue.toString().slice(0, -1);
+        this.printValue();
+    }
+    off(){
+        this.acumulateValue = '';
+        this.currentValue = '';
+        this.operatorType = undefined
+        this.printValue();
+    }
+    addNumber(number){
+        if(number === '.' && this.currentValue.includes('.')) return
+        this.currentValue = this.currentValue.toString() + number.toString();
+        this.printValue();
+        
+    }
+    printValue(){
+        this.current.textContent = this.currentValue;
+        this.acumulate.textContent = `${this.acumulateValue} ${this.signs[this.operatorType] || ''}`;
+
+    }
+    calculate(){
+        const acumulateValue = parseFloat(this.acumulateValue);
+        const currentValue = parseFloat(this.currentValue);
+        if(isNaN(acumulateValue) || isNaN(currentValue)) return;
+        this.currentValue = this.calculator[this.operatorType](acumulateValue, currentValue)
+
+    }
+    
+}
+const display = new Display(acumulate, current);
